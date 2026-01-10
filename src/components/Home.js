1d3bcd84d478883b53a045ab2c6a3853053c736a
Kratikap23 <kratikap665@gmail.com>
2026-01-10 22:13:07 +0530

    Fetching Notes from Context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import NoteContext from '../context/notes/NoteContext';
 
 const Home = () => {
+  const context = useContext(NoteContext);
+  const {notes, setNotes} = context;
   return (
     <div >
       <div className="container my-3">
         <h2> Add a Note</h2>
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <form>-
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" />
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
           </div>
-          <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" for="exampleCheck1">Check me out</label>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
       <div className="container my-3">
         <h2> Your Notes</h2>
+            {Array.isArray(notes) && notes.map((note) => (
+          <div key={note._id} className="card my-2">
+            <div className="card-body">
+              <h5 className="card-title">{note.title}</h5>
+              <p className="card-text">{note.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,11 +3,31 @@ import NoteContext from "./NoteContext";
 
 
 const NoteState=({children})=> {
-   
+   const notesInitial =[
+  {
+    "_id": "695e8f99cbf4843c7ae552ba",
+    "user": "695d388b7b2248e5e74ff6f2",
+    "title": "My title",
+    "description": "Please Wake up Early",
+    "tag": "personal",
+    "date": "2026-01-07T16:53:45.879Z",
+    "__v": 0
+  },
+  {
+    "_id": "695faaef6dfe3271d047744e",
+    "user": "695d388b7b2248e5e74ff6f2",
+    "title": "My title",
+    "description": "Please Wake up Early",
+    "tag": "personal",
+    "date": "2026-01-08T13:02:39.364Z",
+    "__v": 0
+  }
+]
+const [notes , setNotes]= useState(notesInitial);
    
     
     return (
-        <NoteContext.Provider  value = {{}}>
+        <NoteContext.Provider  value = {{notes, setNotes}}>
             {children}
         </NoteContext.Provider>
     )
